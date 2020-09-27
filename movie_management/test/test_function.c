9bbb3a8a31ff1eebfb786499276891a053cb9ea6
99002531 <71481297+99002531@users.noreply.github.com>
2020-09-27 17:50:41 +0530

    Update test_function.c

--- movie_management/test/test_function.c
+++ movie_management/test/test_function.c
@@ -7,30 +7,30 @@ void tearDown(){}
 
 void test_insert_details(void)
 {
-  TEST_ASSERT_EQUAL(0, insert_details())    //success
-  
+  TEST_ASSERT_EQUAL(0,insert_details("8.5","go","2016",200);   //success
+
 }
 void test_find(void)
 {
-  TEST_ASSERT_EQUAL(0, find());   //success
-  
+  TEST_ASSERT_EQUAL(0,find("Avengers"));   //success
+
 }
 void test_viewAll(void)
 {
   TEST_ASSERT_EQUAL(0,viewAll()); //sucees
-  
+
 }
 
 void test_subscriptions(void)
 {
-  TEST_ASSERT_EQUAL(0,subscriptions());
-  
+  TEST_ASSERT_EQUAL(0,subscriptions("Avengers","ram","362645959",2));
+
 }
 
 void test_old_record(void)
 {
   TEST_ASSERT_EQUAL(0,old_record());
-  
+
 }
 
 
@@ -45,9 +45,9 @@ int test_main(void)
    RUN_TEST(test_viewAll);
   RUN_TEST(test_subscriptions);
    RUN_TEST(test_old_record);
-  
-  
-  
+
+
+
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
