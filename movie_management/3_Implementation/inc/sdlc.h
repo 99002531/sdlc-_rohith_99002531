133da30920ded1f769b389e34a004c117a9e1256
99002531 <71481297+99002531@users.noreply.github.com>
2020-09-27 17:31:34 +0530

    Update sdlc.h
    header

--- movie_management/3_Implementation/inc/sdlc.h
+++ movie_management/3_Implementation/inc/sdlc.h
@@ -2,20 +2,22 @@
 #include<stdlib.h>
 #include<string.h>
 
-struct book
+struct mystruct
 {
 	char imdb[10];
 	char name[20];
 	char year[10];
+//	char find[20];
 	int cost;
-
-
+	char movie_name[20];
 }b;
 
 
-int insert_details();//for inserting movie details
-int viewAll(); // for view all data
-int find(); // for finding data
-int subscriptions();  //for geting subscriptions
-int old_record(); //for view old recorrds of users,booked tickets
+
+
+void insert_details(char imdb[],char name[],char year[],int cost) ;//for inserting movie details
+void viewAll(); // for view all data
+void find(char find[]); // for finding data
+void subscriptions(char movie_name[],char person_name[],char mobile[],int total_subcriptions); //for geting subscriptions
+void old_record(); //for view old records
 
